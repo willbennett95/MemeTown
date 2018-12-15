fe342b72a639e262adf9833524be2c7f65ed70b0
mustakimrowshan <mustakimrowshan@gmail.com>
2018-12-15 21:26:20 +0000

    updated settings.py for db

--- memes/views.py
+++ memes/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'page.html')
+    return render(request, 'index.html')
--- memetown/settings.py
+++ memetown/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'memetown.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+		'HOST': '35.197.251.95',
+		'NAME': 'users',
+		'USERNAME': 'mustakimrowshan',
+		'PASSWORD': 'password',
     }
 }
 
