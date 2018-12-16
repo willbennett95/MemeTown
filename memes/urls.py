47508c898c001dbeed4b52d2e07821dbc3df931d
mustakimrowshan <mustakimrowshan@gmail.com>
2018-12-16 17:11:56 +0000

    updated urls.py

--- memes/urls.py
+++ memes/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'memes'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.loginPage, name='loginPage'),
+    url(r'^signup/$', views.signupPage, name='signupPage'),
 ]
--- memes/views.py
+++ memes/views.py
@@ -3,7 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
+# Index Page 
 def index(request):
     return render(request, 'index.html')
+
+# Login Page 
+def loginPage(request):
+    return render(request, 'loginpage.html')
+
+# Signup Page 
+def signupPage(request):
+    return render(request, 'signuppage.html')
